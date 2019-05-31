bde30f0812d91b66d0ecd7cf8124d37688ec8548
Алексей <alex27_piter@mail.ru>
2019-05-31 22:32:30 +0300

    version 1.1

--- src/main/java/sample/core/Operation.java
+++ src/main/java/sample/core/Operation.java
@@ -31,23 +31,35 @@ public enum Operation {
             case PLUS:
             case MINUS:
             case MULTIPLY:
-            case DIVIDE: return true;
-            default: return false;
+            case DIVIDE:
+                return true;
+            default:
+                return false;
         }
     }
 
     int numberOf() {
         switch (this) {
-            case ONE: return 1;
-            case TWO: return 2;
-            case THREE: return 3;
-            case FOUR: return 4;
-            case FIVE: return 5;
-            case SIX: return 6;
-            case SEVEN: return 7;
-            case EIGHT: return 8;
-            case NINE: return 9;
-            default: return 0;
+            case ONE:
+                return 1;
+            case TWO:
+                return 2;
+            case THREE:
+                return 3;
+            case FOUR:
+                return 4;
+            case FIVE:
+                return 5;
+            case SIX:
+                return 6;
+            case SEVEN:
+                return 7;
+            case EIGHT:
+                return 8;
+            case NINE:
+                return 9;
+            default:
+                return 0;
         }
     }
 }
